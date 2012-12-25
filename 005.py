7ed00c8106133e0118955feeaa78839027a0eedc
joe di castro <joe@joedicastro.com>
2012-12-25 23:14:27 +0100

    added code for problem 5

---
+++ 005.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+Smallest multiple
+Problem 5
+
+2520 is the smallest number that can be divided by each of the numbers from 1
+to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the
+numbers from 1 to 20?
+"""
+
+
+def evenly_divisible_by(number, from_1_to):
+    for num in range(from_1_to - 1, 1, -1):
+        if number % num != 0:
+            return False
+    return True
+
+numbers_up_to = 20
+smallest = numbers_up_to
+
+while not evenly_divisible_by(smallest, numbers_up_to):
+    smallest += numbers_up_to
+
+print smallest
+
+###########################################################################
+#                                 Answer                                  #
+###########################################################################
+
+# 232792560
---
+++ 005_1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+Smallest multiple
+Problem 5
+
+2520 is the smallest number that can be divided by each of the numbers from 1
+to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the
+numbers from 1 to 20?
+"""
+
+
+# Euclidian algorithm
+def great_common_divisor(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def least_common_multiple(a, b):
+    return a * b / great_common_divisor(a, b)
+
+
+def least_common_multiple_range(lst):
+    return reduce(least_common_multiple, lst)
+
+
+print least_common_multiple_range(range(1, 20))
+
+###########################################################################
+#                                 Answer                                  #
+###########################################################################
+
+# 232792560
