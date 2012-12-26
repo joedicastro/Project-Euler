e44447d6b18ef1afe0f950774ef738ae2148c7b8
joe di castro <joe@joedicastro.com>
2012-12-26 20:15:25 +0100

    added code for the problem 6

---
+++ 006.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+Sum square difference
+Problem 6
+
+The sum of the squares of the first ten natural numbers is,
+12 + 22 + ... + 102 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural
+numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred
+natural numbers and the square of the sum.
+"""
+
+numbers_up_to = 100
+
+sum_of_squares = sum(x ** 2 for x in xrange(1, numbers_up_to + 1))
+square_of_sum = sum(y for y in xrange(1, numbers_up_to + 1)) ** 2
+
+print  square_of_sum - sum_of_squares
+
+###########################################################################
+#                                 Answer                                  #
+###########################################################################
+
+# 25164150
---
+++ 006_1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+Sum square difference
+Problem 6
+
+The sum of the squares of the first ten natural numbers is,
+12 + 22 + ... + 102 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural
+numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred
+natural numbers and the square of the sum.
+"""
+
+numbers_up_to = 100
+nut_plus_one = numbers_up_to + 1
+
+sum_of_squares = ((numbers_up_to * nut_plus_one) / 2) ** 2
+square_of_sum = ((2 * numbers_up_to + 1) * nut_plus_one * numbers_up_to) / 6
+
+print  sum_of_squares - square_of_sum
+
+###########################################################################
+#                                 Answer                                  #
+###########################################################################
+
+# 25164150
