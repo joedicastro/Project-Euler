396437f31093fb0cbf34b0758cb8d9633b844fc5
joe di castro <joe@joedicastro.com>
2012-12-27 19:07:41 +0100

    added code for the problem 7

---
+++ 007.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+10001st prime
+Problem 7
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
+the 6th prime is 13.
+
+What is the 10 001st prime number?
+"""
+
+
+import math
+
+
+def is_prime(number):
+    if number == 2:
+        return True
+    elif number < 2 or number % 2 == 0:
+        return False
+    else:
+        for n in range(2, int(math.sqrt(number)) + 1):
+            if number % n == 0:
+                return False
+        return True
+
+i = 1
+prime_numbers = []
+
+while len(prime_numbers) < 10001:
+    if is_prime(i):
+        prime_numbers.append(i)
+    i += 1
+
+print max(prime_numbers)
+
+###########################################################################
+#                                 Answer                                  #
+###########################################################################
+
+# 104743
---
+++ 007_1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+10001st prime
+Problem 7
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
+the 6th prime is 13.
+
+What is the 10 001st prime number?
+"""
+
+
+import math
+
+
+def is_prime(number):
+    for n in range(2, int(math.sqrt(number)) + 1):
+        if number % n == 0:
+            return False
+    return True
+
+
+i = 1
+# let's assume 2 and 3 as prime numbers by default
+prime_numbers = [2, 3]
+
+while len(prime_numbers) < 10001:
+    for x in (6 * i - 1, 6 * i + 1):
+        if is_prime(x):
+            prime_numbers.append(x)
+    i += 1
+
+print max(prime_numbers)
+
+###########################################################################
+#                                 Answer                                  #
+###########################################################################
+
+# 104743
