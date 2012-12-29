4d70fe097ada5c33f8e984486c639ef368abcada
joe di castro <joe@joedicastro.com>
2012-12-29 20:03:56 +0100

    add code for problem 10

---
+++ 010.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+Summation of primes
+Problem 10
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+
+
+import math
+
+
+def is_prime(number):
+    for n in range(2, int(math.sqrt(number)) + 1):
+        if number % n == 0:
+            return False
+    return True
+
+
+i = 1
+# let's assume 2 and 3 as prime numbers by default
+sum = 5
+
+while 6 * i + 1 < 2000000:
+    for x in (6 * i - 1, 6 * i + 1):
+        if is_prime(x):
+            sum += x
+    i += 1
+
+print sum
+
+###########################################################################
+#                                 Answer                                  #
+###########################################################################
+
+# 142913828922
---
+++ 010_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+Summation of primes
+Problem 10
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+
+
+limit = 2000000
+
+primes = [True for n in xrange(0, limit)]
+primes[0], primes[1] = False, False  # 0 and 1 are not primes
+
+for i in xrange(1, int(limit ** 0.5)):
+    if primes[i]:
+        for j in xrange(i ** 2, limit, i):
+            primes[j] = False
+
+sum = 0
+
+for x in xrange(0, len(primes)):
+    if primes[x]:
+        sum += x
+
+print sum
+
+###########################################################################
+#                                 Answer                                  #
+###########################################################################
+
+# 142913828922
